cca327b2be09f93fec30871ae0501d6a744c8309
Antinina Pasichnyk <tonja1@i.ua>
2023-02-02 20:28:37 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,14 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from 'simple-lightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 const galleryEl = document.querySelector('.gallery');
 function newGellery(image) {
     return image
         .map(({preview, original, description}) => {
-            return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a>`
+        return     `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a>`
         })
         .join("");
 }
@@ -19,5 +20,5 @@ new SimpleLightbox('.gallery a', {
     captionsDelay: 250,
 });
 
- console.log(galleryItems);
 console.log(galleryItems);
+
